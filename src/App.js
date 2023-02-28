bb90495a9ab12d810a40889083e4d6febad87cea
kasimurkmez <kasimurkmez47@gmail.com>
2023-02-28 14:45:14 +0300

    Updated

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./App.css";
+import Data from "./Data.json";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+  };
+  return (
+    <div className="container">
+      <h1>Apple Store</h1>
+      <div className="inputContainer">
+        <input
+          className="search"
+          type="text"
+          placeholder="Search for Macbook, iPhone, iPad, Watch, AirPods, and more."
+          onChange={handleChange}
+        />
+      </div>
+      <div className="dataContainer">
+        {Data.filter((apple) => {
+          if (searchTerm === "") {
+            return apple;
+          } else if (
+            apple.title
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return apple;
+          }
+        }).map((apple) => {
+          return (
+            <div className="data" key={apple.id}>
+              <img src={apple.image} alt="" />
+              <h3>{apple.title}</h3>
+              <p>${apple.Price}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
